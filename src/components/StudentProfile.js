883d6df3f6d3328bc7ebb494bc665c005c45030c
d1anas0 <231689@gmail.com>
2025-05-25 22:21:24 +1000

    Update: StudentProfile details to be conditionally rendered

--- src/components/StudentProfile.js
+++ src/components/StudentProfile.js
@@ -1,46 +1,46 @@
-import React, {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import { formatToDDMMYYYY as dateFormatter } from "../dateFormatter";
 import { Dialog } from "@mui/material/";
 
-
 export default function StudentProfile({ selectedStudent, open, closeModal }) {
-    const [details, setDetails] = useState([]);
-    const [birthdate, setBirthdate] = useState(null);
+  const [details, setDetails] = useState([]);
+  const [birthdate, setBirthdate] = useState(null);
 
-    useEffect(() => {
-      if (selectedStudent?.id) {
-        fetch(`http://localhost:5000/students/${selectedStudent.id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            const formattedDate = dateFormatter(data.date_of_birth);
-            setDetails(data);
-            setBirthdate(formattedDate);
-          })
-          // TODO: Error Handling (messages)
-          .catch((error) => {
-            console.error("error", error);
-          });
-        }
-  }, [selectedStudent?.id])
+  useEffect(() => {
+    if (selectedStudent?.id) {
+      fetch(`http://localhost:5000/students/${selectedStudent.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const formattedDate = dateFormatter(data.date_of_birth);
+          setDetails(data);
+          setBirthdate(formattedDate);
+        })
+        // TODO: Error Handling (messages)
+        .catch((error) => {
+          console.error("error", error);
+        });
+    }
+  }, [selectedStudent?.id]);
 
   return (
     // TODO: UI of modal
-    <Dialog
-      maxWidth="sm"
-      open={open}
-      onClose={closeModal}
-    >
+    <Dialog maxWidth="sm" open={open} onClose={closeModal}>
       {/* TODO: Holds profile picture  */}
-
       {/* TODO: First name and Last name fields are editable */}
-        <div>{details.first_name}</div>
-        <div>{details.last_name}</div>
-        <div>{birthdate}</div>
-        <div>{details.address?.street_line1}</div>
-        <div>{details.address?.street_line2}</div>
-        <div>{details.address?.country}</div>
-        <div>{details.address?.postcode}</div>
+      {details.first_name ? (
+        <>
+          <div>{details.first_name}</div>
+          <div>{details.last_name}</div>
+          <div>{birthdate}</div>
+          <div>{details.address?.street_line1}</div>
+          <div>{details.address?.street_line2}</div>
+          <div>{details.address?.country}</div>
+          <div>{details.address?.postcode}</div>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </Dialog>
   );
 }
