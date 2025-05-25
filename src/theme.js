ebc24de04d38470293b460f626998e28469639bc
d1anas0 <231689@gmail.com>
2025-05-25 22:38:49 +1000

    New: add custom theme for project colour palette

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9FBF9B",
+    },
+    secondary: {
+      main: "#199059",
+    },
+    text: {
+      primary: "#0E1B0E",
+    },
+    background: {
+      default: "#EFEDE6",
+    },
+  },
+});
+
+export default theme;
